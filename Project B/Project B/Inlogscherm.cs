234a1e982c7e1bedc2aa525436734206cd29b5c3
Darja <dvidelina@gmail.com>
2020-03-28 13:15:14 +0100

    Login
    Loginscherm, accountscherm. Login functie werkt voer in user: Darja en wachtwoord: 2001

--- Project B/Project B/Inlogscherm.cs
+++ Project B/Project B/Inlogscherm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Project_B
 {
@@ -22,6 +23,36 @@ namespace Project_B
             this.Hide();
             Form1 menu = new Form1();
             menu.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dvide\OneDrive\Documenten\Data.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) from Login where Gebruikersnaam='" + textBox1.Text + "'and Wachtwoord='" + textBox2.Text +"' ", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Account acc = new Account();
+                acc.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Verkeerde combinatie van gebruikersnaam en wachtwoord, probeer opnieuw");
+            }
+        
+        
         }
     }
 }
