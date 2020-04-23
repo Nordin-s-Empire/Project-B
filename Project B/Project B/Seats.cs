3b935d5f2e63c12962e6f3e39270a9346fc18e2f
Tyednut <nikola_1998@live.nl>
2020-04-23 22:32:09 +0200

    Reservatie

---
+++ Project B/Project B/Seats.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_B
+{
+    public partial class StoelenReservatie
+    {
+        class Seats
+        {
+            public string SeatName { get; set; }
+            public bool SeatAvailability { get; set; }
+        }
+    }
+}
