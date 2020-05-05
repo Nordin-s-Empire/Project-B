959b2771dbc8b9b7bf0bc064ccc107cce99c3dd8
KingNordin <60733104+KingNordin@users.noreply.github.com>
2020-05-05 16:25:22 +0200

    fix

--- Project-B/Project B/Reservatie.cs
+++ Project-B/Project B/Reservatie.cs
@@ -1,29 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlTypes;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Data.Sql;
-using System.Data.SqlClient;
-using System.CodeDom;
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Project_B
 {
